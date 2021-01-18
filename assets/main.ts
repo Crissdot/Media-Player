1db50f11d7d29be1f7a187b2a0ba1099685d05d8
crissdot <cris-2-11-2001@hotmail.com>
2021-01-18 13:02:04 -0600

    add Ad Plugin

--- assets/main.ts
+++ assets/main.ts
@@ -1,6 +1,7 @@
 import MediaPlayer from './MediaPlayer';
 import AutoPlay from './Plugins/AutoPlay';
 import AutoPause from './Plugins/AutoPause';
+import Ads from './Plugins/Ads';
 
 const video = document.querySelector("video");
 const playButton: HTMLElement = document.querySelector(".playButton");
@@ -8,7 +9,11 @@ const muteButton: HTMLElement = document.querySelector(".muteButton");
 
 const mediaPlay = {
     el: video,
-    plugins: [new AutoPlay(), new AutoPause()],
+    plugins: [
+        new AutoPlay(),
+        new AutoPause(),
+        new Ads(),
+    ],
 }
 
 const player = new MediaPlayer(mediaPlay);
